6d814ca11c82ff12f86f250444f5a34b9688ab36
Arthurv <ArthurVaverko@gmail.com>
2017-04-02 06:57:48 +0300

    added dist to allow demo on github

---
+++ main.bundle.js
@@ -0,0 +1,260 @@
+webpackJsonp([1,4],{
+
+/***/ 135:
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__(45)();
+// imports
+
+
+// module
+exports.push([module.i, "\r\n\r\n.wrapper{\r\n  max-width: 1200px;\r\n  margin: auto;\r\n}\r\n\r\nh1, h2, h3, h4 {  \r\n  font-weight: 900;\r\n  color: #333;\r\n  margin: 1.5em 0 0.5em;\r\n}\r\n\r\n\r\nh1 {font-size: 2em;}\r\nh2 {font-size: 1.8em;}\r\nh3 {font-size: 1.5em;}\r\nh4 {font-size: 1.3em;}\r\n\r\n\r\n/*Basic Grid Styles*/\r\n.grid {\r\n  display: -webkit-box;\r\n  display: -ms-flexbox;\r\n  display: flex;\r\n  -webkit-box-orient: horizontal;\r\n  -webkit-box-direction: normal;\r\n      -ms-flex-flow: row;\r\n          flex-flow: row; \r\n  -ms-flex-wrap: wrap; \r\n      flex-wrap: wrap;\r\n}\r\n.text-center {\r\n text-align: center;\r\n}\r\n.grid-cell {\r\n  -webkit-box-flex: 1;\r\n      -ms-flex: 1;\r\n          flex: 1;\r\n}\r\n\r\n\r\n.demo {\r\n  padding: .8em 1em 0 0;\r\n  margin: 0 .5em 0 .5em;\r\n  background: rgba(51, 153, 204,.2);\r\n  -webkit-transition: background-color 0.3s ease;\r\n  transition: background-color 0.3s ease;\r\n  border: 1px solid #33cccc;\r\n  border-radius: 3px;\r\n}\r\n.demo:hover {\r\n  background: rgba(51, 153, 204,.6);\r\n}\r\n", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ 137:
+/***/ (function(module, exports) {
+
+module.exports = "<h1>\n  Fill-Height Directive Example\n</h1>\n\n<div class=\"grid\">\n\t<div class=\"grid-cell\">\n\t\t<div class=\"demo\" fill-height>\n\t\t\t<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean posuere vulputate nulla non suscipit. Etiam sed viverra ligula. Fusce a ligula elementum, pharetra est quis, ultrices magna. Cras blandit nisi ut tempus hendrerit. Donec semper erat id vulputate lobortis. Etiam suscipit lacinia quam, et malesuada ante accumsan eu. Nulla facilisi. Vivamus interdum est feugiat tempus lacinia. Nulla sit amet blandit mauris, sit amet bibendum ante. Sed pretium urna nulla, sit amet varius sem posuere sit amet.</p>\n\t\t\t<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean posuere vulputate nulla non suscipit. Etiam sed viverra ligula. Fusce a ligula elementum, pharetra est quis, ultrices magna. Cras blandit nisi ut tempus hendrerit. Donec semper erat id vulputate lobortis. Etiam suscipit lacinia quam, et malesuada ante accumsan eu. Nulla facilisi. Vivamus interdum est feugiat tempus lacinia. Nulla sit amet blandit mauris, sit amet bibendum ante. Sed pretium urna nulla, sit amet varius sem posuere sit amet.</p>\n\t\t\t<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean posuere vulputate nulla non suscipit. Etiam sed viverra ligula. Fusce a ligula elementum, pharetra est quis, ultrices magna. Cras blandit nisi ut tempus hendrerit. Donec semper erat id vulputate lobortis. Etiam suscipit lacinia quam, et malesuada ante accumsan eu. Nulla facilisi. Vivamus interdum est feugiat tempus lacinia. Nulla sit amet blandit mauris, sit amet bibendum ante. Sed pretium urna nulla, sit amet varius sem posuere sit amet.</p>\n\t\t\t<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean posuere vulputate nulla non suscipit. Etiam sed viverra ligula. Fusce a ligula elementum, pharetra est quis, ultrices magna. Cras blandit nisi ut tempus hendrerit. Donec semper erat id vulputate lobortis. Etiam suscipit lacinia quam, et malesuada ante accumsan eu. Nulla facilisi. Vivamus interdum est feugiat tempus lacinia. Nulla sit amet blandit mauris, sit amet bibendum ante. Sed pretium urna nulla, sit amet varius sem posuere sit amet.</p>\n\t\t\t<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean posuere vulputate nulla non suscipit. Etiam sed viverra ligula. Fusce a ligula elementum, pharetra est quis, ultrices magna. Cras blandit nisi ut tempus hendrerit. Donec semper erat id vulputate lobortis. Etiam suscipit lacinia quam, et malesuada ante accumsan eu. Nulla facilisi. Vivamus interdum est feugiat tempus lacinia. Nulla sit amet blandit mauris, sit amet bibendum ante. Sed pretium urna nulla, sit amet varius sem posuere sit amet.</p>\n\t\t\t<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean posuere vulputate nulla non suscipit. Etiam sed viverra ligula. Fusce a ligula elementum, pharetra est quis, ultrices magna. Cras blandit nisi ut tempus hendrerit. Donec semper erat id vulputate lobortis. Etiam suscipit lacinia quam, et malesuada ante accumsan eu. Nulla facilisi. Vivamus interdum est feugiat tempus lacinia. Nulla sit amet blandit mauris, sit amet bibendum ante. Sed pretium urna nulla, sit amet varius sem posuere sit amet.</p>\n\t\t\t<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean posuere vulputate nulla non suscipit. Etiam sed viverra ligula. Fusce a ligula elementum, pharetra est quis, ultrices magna. Cras blandit nisi ut tempus hendrerit. Donec semper erat id vulputate lobortis. Etiam suscipit lacinia quam, et malesuada ante accumsan eu. Nulla facilisi. Vivamus interdum est feugiat tempus lacinia. Nulla sit amet blandit mauris, sit amet bibendum ante. Sed pretium urna nulla, sit amet varius sem posuere sit amet.</p>\n\t\t\t<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean posuere vulputate nulla non suscipit. Etiam sed viverra ligula. Fusce a ligula elementum, pharetra est quis, ultrices magna. Cras blandit nisi ut tempus hendrerit. Donec semper erat id vulputate lobortis. Etiam suscipit lacinia quam, et malesuada ante accumsan eu. Nulla facilisi. Vivamus interdum est feugiat tempus lacinia. Nulla sit amet blandit mauris, sit amet bibendum ante. Sed pretium urna nulla, sit amet varius sem posuere sit amet.</p>\n\t\t\t<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean posuere vulputate nulla non suscipit. Etiam sed viverra ligula. Fusce a ligula elementum, pharetra est quis, ultrices magna. Cras blandit nisi ut tempus hendrerit. Donec semper erat id vulputate lobortis. Etiam suscipit lacinia quam, et malesuada ante accumsan eu. Nulla facilisi. Vivamus interdum est feugiat tempus lacinia. Nulla sit amet blandit mauris, sit amet bibendum ante. Sed pretium urna nulla, sit amet varius sem posuere sit amet.</p>\n\t\t</div>\n\t</div>\n\t<div class=\"grid-cell\">\n\t\t<div class=\"demo\">\n\t\t\t<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean posuere vulputate nulla non suscipit. Etiam sed viverra ligula. Fusce a ligula elementum, pharetra est quis, ultrices magna. Cras blandit nisi ut tempus hendrerit. Donec semper erat id vulputate lobortis. Etiam suscipit lacinia quam, et malesuada ante accumsan eu. Nulla facilisi. Vivamus interdum est feugiat tempus lacinia. Nulla sit amet blandit mauris, sit amet bibendum ante. Sed pretium urna nulla, sit amet varius sem posuere sit amet.</p>\n\t\t</div>\n\t</div>\n\t<div class=\"grid-cell\">\n\t\t<div class=\"demo\">\n\t\t\t<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean posuere vulputate nulla non suscipit. Etiam sed viverra ligula. Fusce a ligula elementum, pharetra est quis, ultrices magna. Cras blandit nisi ut tempus hendrerit. Donec semper erat id vulputate lobortis. Etiam suscipit lacinia quam, et malesuada ante accumsan eu. Nulla facilisi. Vivamus interdum est feugiat tempus lacinia. Nulla sit amet blandit mauris, sit amet bibendum ante. Sed pretium urna nulla, sit amet varius sem posuere sit amet.</p>\n\t\t</div>\n\t</div>\n</div>\n"
+
+/***/ }),
+
+/***/ 162:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(70);
+
+
+/***/ }),
+
+/***/ 69:
+/***/ (function(module, exports) {
+
+function webpackEmptyContext(req) {
+	throw new Error("Cannot find module '" + req + "'.");
+}
+webpackEmptyContext.keys = function() { return []; };
+webpackEmptyContext.resolve = webpackEmptyContext;
+module.exports = webpackEmptyContext;
+webpackEmptyContext.id = 69;
+
+
+/***/ }),
+
+/***/ 70:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(3);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__ = __webpack_require__(76);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__app_app_module__ = __webpack_require__(78);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__environments_environment__ = __webpack_require__(81);
+
+
+
+
+if (__WEBPACK_IMPORTED_MODULE_3__environments_environment__["a" /* environment */].production) {
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["a" /* enableProdMode */])();
+}
+__webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__["a" /* platformBrowserDynamic */])().bootstrapModule(__WEBPACK_IMPORTED_MODULE_2__app_app_module__["a" /* AppModule */]);
+//# sourceMappingURL=main.js.map
+
+/***/ }),
+
+/***/ 77:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(3);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppComponent; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+var AppComponent = (function () {
+    function AppComponent() {
+    }
+    return AppComponent;
+}());
+AppComponent = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_4" /* Component */])({
+        selector: 'app-root',
+        template: __webpack_require__(137),
+        styles: [__webpack_require__(135)]
+    })
+], AppComponent);
+
+//# sourceMappingURL=app.component.js.map
+
+/***/ }),
+
+/***/ 78:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__ = __webpack_require__(20);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_core__ = __webpack_require__(3);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__angular_forms__ = __webpack_require__(74);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__angular_http__ = __webpack_require__(75);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__app_component__ = __webpack_require__(77);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__fill_height_fill_height_module__ = __webpack_require__(80);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppModule; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+
+
+
+var AppModule = (function () {
+    function AppModule() {
+    }
+    return AppModule;
+}());
+AppModule = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__angular_core__["b" /* NgModule */])({
+        declarations: [
+            __WEBPACK_IMPORTED_MODULE_4__app_component__["a" /* AppComponent */],
+        ],
+        imports: [
+            __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */],
+            __WEBPACK_IMPORTED_MODULE_2__angular_forms__["a" /* FormsModule */],
+            __WEBPACK_IMPORTED_MODULE_3__angular_http__["a" /* HttpModule */],
+            __WEBPACK_IMPORTED_MODULE_5__fill_height_fill_height_module__["a" /* FillHeightModule */],
+        ],
+        providers: [],
+        bootstrap: [__WEBPACK_IMPORTED_MODULE_4__app_component__["a" /* AppComponent */]]
+    })
+], AppModule);
+
+//# sourceMappingURL=app.module.js.map
+
+/***/ }),
+
+/***/ 79:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(3);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return FillHeightDirective; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+var FillHeightDirective = (function () {
+    function FillHeightDirective(el) {
+        this.el = el;
+    }
+    FillHeightDirective.prototype.ngAfterViewInit = function () {
+        this.calculateAndSetElementHeight();
+    };
+    FillHeightDirective.prototype.onResize = function (event) {
+        this.calculateAndSetElementHeight();
+    };
+    FillHeightDirective.prototype.calculateAndSetElementHeight = function () {
+        this.el.nativeElement.style.overflow = 'auto';
+        var windowHeight = window.innerHeight;
+        var elementOffsetTop = this.el.nativeElement.offsetTop;
+        var elementMarginBottom = this.el.nativeElement.style.marginBottom;
+        this.el.nativeElement.style.height = windowHeight - elementOffsetTop + 'px';
+        console.log('Fill-Height.Directive (Recalculatating height):', {
+            windowHeight: windowHeight,
+            elementOffsetTop: elementOffsetTop,
+            elementMarginBottom: elementMarginBottom,
+            finalHeihgt: windowHeight - elementOffsetTop,
+        });
+    };
+    return FillHeightDirective;
+}());
+__decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["c" /* HostListener */])('window:resize', ['$event']),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [Object]),
+    __metadata("design:returntype", void 0)
+], FillHeightDirective.prototype, "onResize", null);
+FillHeightDirective = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["d" /* Directive */])({ selector: '[fill-height]' }),
+    __metadata("design:paramtypes", [typeof (_a = typeof __WEBPACK_IMPORTED_MODULE_0__angular_core__["e" /* ElementRef */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_0__angular_core__["e" /* ElementRef */]) === "function" && _a || Object])
+], FillHeightDirective);
+
+var _a;
+//# sourceMappingURL=fill-height.directive.js.map
+
+/***/ }),
+
+/***/ 80:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(3);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_common__ = __webpack_require__(28);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__fill_height_directive__ = __webpack_require__(79);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return FillHeightModule; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+var FillHeightModule = (function () {
+    function FillHeightModule() {
+    }
+    return FillHeightModule;
+}());
+FillHeightModule = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["b" /* NgModule */])({
+        imports: [
+            __WEBPACK_IMPORTED_MODULE_1__angular_common__["a" /* CommonModule */]
+        ],
+        declarations: [__WEBPACK_IMPORTED_MODULE_2__fill_height_directive__["a" /* FillHeightDirective */]],
+        exports: [__WEBPACK_IMPORTED_MODULE_2__fill_height_directive__["a" /* FillHeightDirective */]]
+    })
+], FillHeightModule);
+
+//# sourceMappingURL=fill-height.module.js.map
+
+/***/ }),
+
+/***/ 81:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return environment; });
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+// The file contents for the current environment will overwrite these during build.
+var environment = {
+    production: false
+};
+//# sourceMappingURL=environment.js.map
+
+/***/ })
+
+},[162]);
+//# sourceMappingURL=main.bundle.js.map
